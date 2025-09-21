0fde121fed22e2bab7ce9febfb97b529ee4572d2
MohassinHussain <129653830+MohassinHussain@users.noreply.github.com>
2025-09-21 15:28:44 +0530

    Update Card2.js

--- app/components/Card2.js
+++ app/components/Card2.js
@@ -4,7 +4,7 @@ function Card2() {
   return (
     <div className="bg-[#1a1b26] w-[90%] sm:w-[80%] md:w-[80rem] m-5 h-auto sm:h-auto rounded-md mx-auto border border-[#24283b]">
       <h1 className="text-4xl sm:text-5xl p-6 sm:p-10 font-bold text-center text-[#c0caf5]">About & Work</h1>
-    <a href="https://discord.gg/bakEVs9w">Join Discord</a>
+    // <a href="https://discord.gg/bakEVs9w">Join Discord</a>
       <div className="grid grid-cols-1 sm:grid-cols-2 justify-around gap-6 m-6 sm:m-8">
         
         <div className="bg-[#24283b] p-6 sm:p-8 border border-[#24283b] rounded-lg">
