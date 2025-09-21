ea64ee03328c0db4ba4181f921ed5b2487e2316e
MohassinHussain <129653830+MohassinHussain@users.noreply.github.com>
2025-09-21 14:40:13 +0530

    Update Card6.js

--- app/components/Card6.js
+++ app/components/Card6.js
@@ -8,7 +8,7 @@ function Card6() {
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 justify-around m-6 gap-6">
         {/* View My Resume Card */}
-        <a target="_blank" href="https://drive.google.com/file/d/1GkPkQXKovbtFFfvXGfocAp0VD5DEqVwz/view?usp=drivesdk" className="p-6 bg-[#24283b] rounded-lg border border-[#24283b] hover:bg-[#7aa2f7] transition-all duration-300 transform hover:scale-105 shadow-md" >
+        <a target="_blank" href="https://drive.google.com/file/d/149P1GfROb4EqX0wLp0CN8KPOfpWdXDUu/view?usp=sharing" className="p-6 bg-[#24283b] rounded-lg border border-[#24283b] hover:bg-[#7aa2f7] transition-all duration-300 transform hover:scale-105 shadow-md" >
           <p className="text-[1.5rem] text-center text-[#c0caf5] hover:text-[#1a1b26] transition-all duration-300 transform hover:scale-105">
             View My Resume
           </p>
