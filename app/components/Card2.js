ac332c8f417a4ae0a72f098b9ee58e6a803e9c71
MohassinHussain <129653830+MohassinHussain@users.noreply.github.com>
2025-09-21 14:46:22 +0530

    Update Card2.js

--- app/components/Card2.js
+++ app/components/Card2.js
@@ -5,6 +5,7 @@ function Card2() {
     <div className="bg-[#1a1b26] w-[90%] sm:w-[80%] md:w-[80rem] m-5 h-auto sm:h-auto rounded-md mx-auto border border-[#24283b]">
       <h1 className="text-4xl sm:text-5xl p-6 sm:p-10 font-bold text-center text-[#c0caf5]">About & Work</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 justify-around gap-6 m-6 sm:m-8">
+        <a href="https://discord.gg/bakEVs9w">Join Discord</a>
         <div className="bg-[#24283b] p-6 sm:p-8 border border-[#24283b] rounded-lg">
           <p className="text-[1rem] sm:text-[1.2rem] text-[#a9b1d6]">
           A passionate full-stack developer, AI/ML enthusiast, and problem solver, currently pursuing a B.Tech in AI & ML. Dedicated to innovation, open-source contributions, and building impactful tech solutions.
